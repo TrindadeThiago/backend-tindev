b35d63bde3e58c0a17e8b319af7b5db93d4c7acf
TrindadeThiago <thiago.trindadexd@gmail.com>
2019-08-10 14:49:20 -0300

    Criação do model e controller do Dev

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 const express = require('express');
-const routes = require('./routes')
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://semana:semana@cluster0-eeztj.mongodb.net/tindev?retryWrites=true&w=majority',{
+  useNewUrlParser: true
+})
+
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
